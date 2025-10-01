2c846ffb993bb68f202e321db996d3ef5f634a2d
Aleksey Minchev <95545432+a1exminchev@users.noreply.github.com>
2025-10-01 07:21:16 -0700

    Create App.xaml.cs

---
+++ BIMinPersonalCRM/App.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+
+namespace BIMinPersonalCRM
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml. This partial class is generated when compiling
+    ///     the XAML markup defined in App.xaml. It can be used to handle global
+    ///     application events such as Startup and Exit if needed.
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
