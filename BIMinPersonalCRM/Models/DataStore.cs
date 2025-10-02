0fa989e84a7eae60af69ed390868b80ea95e45aa
Aleksey Minchev <95545432+a1exminchev@users.noreply.github.com>
2025-10-02 05:19:58 -0700

    Update DataStore.cs

--- BIMinPersonalCRM/Models/DataStore.cs
+++ BIMinPersonalCRM/Models/DataStore.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 namespace BIMinPersonalCRM.Models
 {
     /// <summary>
-    ///     Represents the root object persisted to the JSON file. It
-    ///     aggregates collections of the domain models used by the application.
+    /// Represents the root object persisted to the JSON file.
+    /// Aggregates collections of domain models used by the application.
     /// </summary>
     public class DataStore
     {
         /// <summary>
-        ///     Gets or sets the list of clients recorded in the system.
+        /// Gets or sets the list of companies recorded in the system.
         /// </summary>
-        public List<Client> Clients { get; set; } = new();
+        public List<Company> Companies { get; set; } = new();
 
         /// <summary>
-        ///     Gets or sets the list of tasks recorded in the system.
+        /// Gets or sets the ID of the currently selected task for tracking time.
         /// </summary>
-        public List<TaskItem> Tasks { get; set; } = new();
+        public int? CurrentTaskId { get; set; }
     }
 }
